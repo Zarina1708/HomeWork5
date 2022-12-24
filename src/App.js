f693c40db8f027e7b68324931950ac0fc5e4e362
Zarina1708 <115084290+Zarina1708@users.noreply.github.com>
2022-12-24 15:31:54 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+
+function Change() {
+  const [color,setColor] = useState('blue')
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        {
+          color === "blue" 
+          ? 
+          <button 
+            className='blue' 
+            onClick={() => setColor('red')}>
+              синий
+          </button> 
+          : 
+          <button 
+            className='red' 
+            onClick={() => setColor('blue')}>
+              красный
+          </button>
+        }
+      </div>
     </div>
   );
+  
 }
 
-export default App;
+export default Change;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const App = () => {
+//   const shovHide = () => {
+//       let button = document.getElementById('blue')
+//       if (button.style.display === 'none') {
+//           button.style.display = 'block'
+//       } else if (button.style.display === 'block') {
+//           button.style.display = 'none'
+//       } 
+      
+
+
+//       let button2 = document.getElementById('green')
+//       if (button2.style.display === 'none') {
+//           button2.style.display = 'block'
+//       } else if (button2.style.display === 'block') {
+//           button2.style.display = 'none'
+//       }
+
+      
+      
+//   }
+
+//   return (
+//       <div>
+//           <button id='green' onClick={shovHide} style={{width: '250px', height: '250px', backgroundColor: 'green', marginBottom: '50px'}}>
+//               green
+//           </button>
+//           <button id='blue' onClick={shovHide} style={{ display: 'none', width: '250px', height: '250px', backgroundColor: 'blue' }}>blue</button>
+//       </div>
+//   )
+// }
+
+// export default App;
+
+  
+
